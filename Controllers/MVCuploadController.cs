0193f151e5b9503dff40baafc4a24c0db9c1aa8c
rohan1809 <41717648+rohan1809@users.noreply.github.com>
2020-03-14 20:01:32 +0530

    Add files via upload

---
+++ Controllers/MVCuploadController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Video_Upload_MVC.Models;
+
+namespace Video_Upload_MVC.Controllers
+{
+   
+    public class MVCuploadController : Controller
+    {
+        videoEntities db = new videoEntities();
+        // GET: MVCupload
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(videofile vd , HttpPostedFileBase videofile)
+        {
+            if(videofile!=null)
+            {
+                string filename = Path.GetFileName(videofile.FileName);
+                if(videofile.ContentLength<104857600)
+                {
+                    videofile.SaveAs(Server.MapPath("/Videofiles/"+filename));
+                    vd.Vname = filename;
+                    vd.Vpath = ("/Videofiles/"+filename);
+                    db.videofiles.Add(vd);
+                    db.SaveChanges();
+                }
+            }
+            return RedirectToAction("Display");
+        }
+        public ActionResult Display()
+        {
+            var emp = db.videofiles.ToList();
+            return View(emp);
+        }
+
+
+    }
+}
